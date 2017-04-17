82c010e239f0e56a3e94fff04c4ad9f052fc5963
sehnis <sehnis@umich.edu>
2017-04-17 07:01:16 -0400

    Add caching to improve speed

--- 206_data_access.py
+++ 206_data_access.py
@@ -47,15 +47,28 @@ except:
 
 def build_state_directory():
 
-	# Scrape the data from the dropdown using Requests and BeautifulSoup.
-	state_request = requests.get("https://www.nps.gov/index.htm").text
-	state_soup = BeautifulSoup(state_request, "html.parser")
-	found_states = state_soup.find("ul", {"role" : "menu"})
+	# CHECK CACHE
+	if "cached_states" in CACHE_DICTION:
+		print("Using cached data for the states...")
+		found_urls = CACHE_DICTION["cached_states"]
+	else:
+		print("Retrieving data for the states...")
+		# Scrape the data from the dropdown using Requests and BeautifulSoup.
+		state_request = requests.get("https://www.nps.gov/index.htm").text
+		state_soup = BeautifulSoup(state_request, "html.parser")
+		found_states = state_soup.find("ul", {"role" : "menu"})
 
-	# Get the partial urls from the list's "href" elements, and create full urls from them.
-		# Example: "/state/al/index.htm"  -->  "https://www.nps.gov/state/al/index.htm"
-	raw_urls = [partial["href"] for partial in found_states.find_all("a")]
-	found_urls = ["https://www.nps.gov" + raw for raw in raw_urls]
+		# Get the partial urls from the list's "href" elements, and create full urls from them.
+			# Example: "/state/al/index.htm"  -->  "https://www.nps.gov/state/al/index.htm"
+		raw_urls = [partial["href"] for partial in found_states.find_all("a")]
+		found_urls = ["https://www.nps.gov" + raw for raw in raw_urls]
+
+		# If the program hit this else loop, then the cache doesn't exist and needs to be made like this.
+		# This should save a considerable amount of time instead of rescraping every time.
+		CACHE_DICTION["cached_states"] = found_urls
+		f = open(CACHE_FILENAME, "w")
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
 
 	return found_urls
 
@@ -69,22 +82,64 @@ def build_park_directory():
 	# A list of url lists is needed to return all of the information produced by this function.
 	all_parks = []
 
-	# Iterate through the urls found in build_state_directory(), and set up Requests and BeautifulSoup for each entry.
-	state_urls = build_state_directory()
-	for state in state_urls:
-		parks_request = requests.get(state).text
-		parks_soup = BeautifulSoup(parks_request, "html.parser")
+	if"cached_parks" in CACHE_DICTION:
+		print("Using cached data for the parks...")
+		all_parks = CACHE_DICTION["cached_parks"]
+	else:
+		print("Retrieving data for the parks...")
+		# Iterate through the urls found in build_state_directory(), and set up Requests and BeautifulSoup for each entry.
+		state_urls = build_state_directory()
+		for state in state_urls:
+			parks_request = requests.get(state).text
+			parks_soup = BeautifulSoup(parks_request, "html.parser")
 
-		# Get the individual parks' urls for the given state.
-		park_data = parks_soup.find("ul", {"class" : "list_parks"})
-		raw_urls = [partial["href"] for partial in park_data.find_all("a")]
-		found_urls = ["https://www.nps.gov" + raw + "index.htm" for raw in raw_urls]
+			# Get the individual parks' urls for the given state.
+			# This collection gave errors when shortened, so the lists' code is comparatively expanded.
+			park_data = parks_soup.find("div", {"class" : "ColumnMain col-sm-12"})
+			raw_parks = park_data.find_all("h3")
+			raw_entries = [park.find("a") for park in raw_parks]
+			raw_urls = [entry["href"] for entry in raw_entries]
+			found_urls = ["https://www.nps.gov" + raw + "index.htm" for raw in raw_urls]
 
-		# Add the list of found urls to the overarching list of park urls.
-		all_parks.append(found_urls)
+			# Add the list of found urls to the overarching list of park urls.
+			all_parks.append(found_urls)
+
+		# If the program hit this else loop, then the cache doesn't exist and needs to be made like this.
+		# This should save a considerable amount of time instead of rescraping every time.
+		CACHE_DICTION["cached_parks"] = all_parks
+		f = open(CACHE_FILENAME, "w")
+		f.write(json.dumps(CACHE_DICTION))
+		f.close()
 
 	# When done iterating, return the master list of urls.
 	return all_parks
+
+# CREATE NATIONALPARK OBJECTS -- Use the state urls to initialize the states.
+	
+	# At this point, using BeautifulSoup on the park urls gets us all the information we need for the individual parks.
+	# Adding them directly to an __init__ function is the most efficient, so the NationalPark object is declared now.
+
+# Create a list of NationalPark objects.
+all_urls = build_park_directory()
+full_np_objects = []
+
+for parks_per_state in all_urls:
+	for individual_park in parks_per_state:
+		print(individual_park + "\n")
+	print("\n")
+
+
+#class NationalPark():
+#	def __init__(self):
+		# self.name = TODO
+		# self.location = TODO
+		# self.states = TODO
+		# self.access = TODO
+		# self.contact = TODO
+		# self.urls = TODO
+
+
+
 
 
 # Put your tests here, with any edits you now need from when you turned them in with your project plan.
