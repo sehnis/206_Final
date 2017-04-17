99343d1255a4ecead4c90a810486a2fcb32b9c30
sehnis <sehnis@umich.edu>
2017-04-17 05:05:21 -0400

    Initial commit

---
+++ 206_data_access.py
@@ -0,0 +1,31 @@
+###### INSTRUCTIONS ###### 
+
+# An outline for preparing your final project assignment is in this file.
+
+# Below, throughout this file, you should put comments that explain exactly what you should do for each step of your project. You should specify variable names and processes to use. For example, "Use dictionary accumulation with the list you just created to create a dictionary called tag_counts, where the keys represent tags on flickr photos and the values represent frequency of times those tags occur in the list."
+
+# You can use second person ("You should...") or first person ("I will...") or whatever is comfortable for you, as long as you are clear about what should be done.
+
+# Some parts of the code should already be filled in when you turn this in:
+# - At least 1 function which gets and caches data from 1 of your data sources, and an invocation of each of those functions to show that they work 
+# - Tests at the end of your file that accord with those instructions (will test that you completed those instructions correctly!)
+# - Code that creates a database file and tables as your project plan explains, such that your program can be run over and over again without error and without duplicate rows in your tables.
+# - At least enough code to load data into 1 of your dtabase tables (this should accord with your instructions/tests)
+
+######### END INSTRUCTIONS #########
+
+# Put all import statements you need here.
+
+# Begin filling in instructions....
+
+
+
+
+
+
+
+
+# Put your tests here, with any edits you now need from when you turned them in with your project plan.
+
+
+# Remember to invoke your tests so they will run! (Recommend using the verbosity=2 argument.)
