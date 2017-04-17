a14d28ec8da5403207b0cce83ee05a89e3a6fa1b
sehnis <sehnis@umich.edu>
2017-04-17 06:24:42 -0400

    Initial versions of url gathering

--- 206_data_access.py
+++ 206_data_access.py
@@ -26,11 +26,65 @@ import unittest
 
 # Begin filling in instructions....
 
+# SET UP CACHING -- Creates the json file to cache data.
+
+CACHE_FILENAME = "206_final_cache.json" 
+try:
+	# Use the json cache, if it already exists.
+	f = open(CACHE_FILENAME,'r')
+	cache_data = f.read()
+	f.close()
+	CACHE_DICTION = json.loads(cache_data)
+except:
+	# If it doesn't exist, create a new one.
+	CACHE_DICTION = {}
 
 
+# BUILD STATE DIRECTORY -- The parks are divided by their state, so this collects the state information.
 
+	# There is a dropdown list on the index page, within which all of the state urls can be found.
+	# This function pulls data from it, and generates a list that later functions can use to pull individual state data from.
 
+def build_state_directory():
 
+	# Scrape the data from the dropdown using Requests and BeautifulSoup.
+	state_request = requests.get("https://www.nps.gov/index.htm").text
+	state_soup = BeautifulSoup(state_request, "html.parser")
+	found_states = state_soup.find("ul", {"role" : "menu"})
+
+	# Get the partial urls from the list's "href" elements, and create full urls from them.
+		# Example: "/state/al/index.htm"  -->  "https://www.nps.gov/state/al/index.htm"
+	raw_urls = [partial["href"] for partial in found_states.find_all("a")]
+	found_urls = ["https://www.nps.gov" + raw for raw in raw_urls]
+
+	return found_urls
+
+# BUILD PARK DIRECTORY -- This collects data on the individual parks.
+
+	# Now that we have a list of state urls, we can use BeautifulSoup to individually scrape the park entries for that state.
+	# This function pulls data from the state pages, and creates a number of lists.
+
+def build_park_directory():
+
+	# A list of url lists is needed to return all of the information produced by this function.
+	all_parks = []
+
+	# Iterate through the urls found in build_state_directory(), and set up Requests and BeautifulSoup for each entry.
+	state_urls = build_state_directory()
+	for state in state_urls:
+		parks_request = requests.get(state).text
+		parks_soup = BeautifulSoup(parks_request, "html.parser")
+
+		# Get the individual parks' urls for the given state.
+		park_data = parks_soup.find("ul", {"class" : "list_parks"})
+		raw_urls = [partial["href"] for partial in park_data.find_all("a")]
+		found_urls = ["https://www.nps.gov" + raw + "index.htm" for raw in raw_urls]
+
+		# Add the list of found urls to the overarching list of park urls.
+		all_parks.append(found_urls)
+
+	# When done iterating, return the master list of urls.
+	return all_parks
 
 
 # Put your tests here, with any edits you now need from when you turned them in with your project plan.
